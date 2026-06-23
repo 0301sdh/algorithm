bc4605e37397785000c1ca7a419d3441b3e20dbc
DongHwan Seo <0301sdh@naver.com>
2026-06-23 23:04:52 +0900

    make sort algorithm with java

---
+++ Sort/InsertionSort.java
@@ -0,0 +1,19 @@
+package Sort;
+
+public class InsertionSort {
+
+    public static void insertionsort(int[] arr){
+        int n = arr.length;
+
+        for(int i =1; i<n; i++){
+            int target = arr[i];
+            int j = i-1;
+
+            while (j>=0 && arr[j] > target) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = target;
+        }
+    }
+}
