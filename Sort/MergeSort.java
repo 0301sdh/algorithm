68db12e1527aa54622dd5e0e8b04a3a21b97377c
DongHwan Seo <0301sdh@naver.com>
2026-06-29 18:25:58 +0900

    reuse temp array in MergeSort instead of allocating per merge

--- Sort/MergeSort.java
+++ Sort/MergeSort.java
@@ -1,37 +1,53 @@
 package Sort;
 
 public class MergeSort {
-    
-    public static void mergeSort(int[] A,int p,int r){
-        if(p<r){
-            int q = (p+r)/2;
-            mergeSort(A, p, q);
-            mergeSort(A, q+1, r);
-            merge(A,p,q,r);
+
+    // tmp를 한번만 호출하게 refactoring
+    public static void mergeSort(int[] A) {
+        if (A == null || A.length < 2) {
+            return;
+        }
+
+        int[] tmp = new int[A.length];
+
+        mergeSort(A, 0, A.length - 1, tmp);
+    }
+
+    private static void mergeSort(int[] A, int p, int r, int[] tmp) {
+        if (p < r) {
+            int q = (p + r) / 2;
+            mergeSort(A, p, q, tmp);
+            mergeSort(A, q + 1, r, tmp);
+            merge(A, p, q, r, tmp);
         }
     }
 
-    public static void merge(int[] A, int p, int q, int r){
+    private static void merge(int[] A, int p, int q, int r, int[] tmp) {
         int i = p;
-        int j = q+1;
+        int j = q + 1;
         int k = p;
-        int[] tmp = new int[A.length];
-        while(i<=q && j<=r){
-            if(A[i]<=A[j]){
-                tmp[k++] =A[i++];
-            }
-            else{
+        while (i <= q && j <= r) {
+            if (A[i] <= A[j]) {
+                tmp[k++] = A[i++];
+            } else {
                 tmp[k++] = A[j++];
             }
         }
-        while(i<=q){
-            tmp[k++] =A[i++];
+        while (i <= q) {
+            tmp[k++] = A[i++];
         }
-        while (j<=r) {
-             tmp[k++] =A[j++];
+        while (j <= r) {
+            tmp[k++] = A[j++];
         }
-        for(int g = p; g<=r; g++){
+        for (int g = p; g <= r; g++) {
             A[g] = tmp[g];
         }
+    }
+
+    public static void main(String[] args) {
+        int[] data = { 9, 3, 7, 1, 8, 2, 5, 3, 6, 0 };
+        System.out.println(java.util.Arrays.toString(data));
+        mergeSort(data);
+        System.out.println(java.util.Arrays.toString(data));
     }
 }
