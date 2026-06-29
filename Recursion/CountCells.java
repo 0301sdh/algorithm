6c44b5c69263b64ccb168ff86effc278cae086ab
DongHwan Seo <0301sdh@naver.com>
2026-06-29 17:18:32 +0900

    study and make QuickSort with java

--- Recursion/CountCells.java
+++ Recursion/CountCells.java
@@ -3,42 +3,43 @@ package Recursion;
 public class CountCells {
     public static final int N = 8;
     public static int grid[][] = {
-        {1,0,0,0,0,0,0,1},
-        {0,1,1,0,0,1,0,0},
-        {1,1,0,0,1,0,1,0},
-        {0,0,0,0,0,1,0,0},
-        {0,1,0,1,0,1,0,0},
-        {0,1,0,1,0,1,0,0},
-        {1,0,0,0,1,0,0,1},
-        {0,1,1,0,0,1,1,1}
+            { 1, 0, 0, 0, 0, 0, 0, 1 },
+            { 0, 1, 1, 0, 0, 1, 0, 0 },
+            { 1, 1, 0, 0, 1, 0, 1, 0 },
+            { 0, 0, 0, 0, 0, 1, 0, 0 },
+            { 0, 1, 0, 1, 0, 1, 0, 0 },
+            { 0, 1, 0, 1, 0, 1, 0, 0 },
+            { 1, 0, 0, 0, 1, 0, 0, 1 },
+            { 0, 1, 1, 0, 0, 1, 1, 1 }
     };
 
-    private static int BACKGROUND_COLOR = 0;
+    // private static int BACKGROUND_COLOR = 0;
     private static int IMAGE_COLOR = 1;
     private static int ALREADY_COUNTED = 2;
 
-    public int countCellsInBlob(int x, int y){
-        if(x<0||y<0||x>=N||y>=N)
+    public int countCellsInBlob(int x, int y) {
+        if (x < 0 || y < 0 || x >= N || y >= N)
             return 0;
-        else if(grid[x][y] != IMAGE_COLOR)
+        else if (grid[x][y] != IMAGE_COLOR)
             return 0;
-        else{
+        else {
             grid[x][y] = ALREADY_COUNTED;
-            return 1+ countCellsInBlob(x-1, y+1) + countCellsInBlob(x, y+1) + countCellsInBlob(x+1, y+1) 
-            + countCellsInBlob(x-1, y) + countCellsInBlob(x+1, y)
-            + countCellsInBlob(x-1, y-1) + countCellsInBlob(x, y-1) + countCellsInBlob(x+1, y-1);
+            return 1 + countCellsInBlob(x - 1, y + 1) + countCellsInBlob(x, y + 1) + countCellsInBlob(x + 1, y + 1)
+                    + countCellsInBlob(x - 1, y) + countCellsInBlob(x + 1, y)
+                    + countCellsInBlob(x - 1, y - 1) + countCellsInBlob(x, y - 1) + countCellsInBlob(x + 1, y - 1);
         }
     }
-    public static void printGrid(){
-        for(int i =0; i<N; i++){
-            for(int j=0; j<N; j++){
+
+    public static void printGrid() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 System.out.print(grid[i][j] + " ");
             }
             System.out.println();
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         CountCells cc = new CountCells();
         System.out.println("before counting");
         printGrid();
---
+++ Sort/QuickSort.java
@@ -0,0 +1,57 @@
+package Sort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class QuickSort {
+
+    private static final Random rand = new Random();
+
+    public static void quickSort(int[] A, int p, int r) {
+        int q;
+        if (p < r) {
+            q = partition2(A, p, r);
+            quickSort(A, p, q - 1);
+            quickSort(A, q + 1, r);
+        }
+    }
+
+    // pivot 을 가장 마지막 데이터로 지정 : 이미 정렬된 상태면 시간 복잡도가 최악임
+    public static int partition(int[] A, int low, int high) {
+        int pivot = A[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (A[j] < pivot) {
+                i++;
+                swap(A, i, j);
+            }
+        }
+        swap(A, i + 1, high);
+        return i + 1;
+    }
+
+    public static int partition2(int[] A, int low, int high) {
+
+        int pivotIndex = low + rand.nextInt(high - low + 1);
+
+        swap(A, pivotIndex, high);
+
+        return partition(A, low, high);
+
+    }
+
+    private static void swap(int[] A, int p, int q) {
+        int temp;
+        temp = A[p];
+        A[p] = A[q];
+        A[q] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] data = { 9, 3, 7, 1, 8, 2, 5, 3, 6, 0 };
+        System.out.println(Arrays.toString(data));
+        quickSort(data, 0, data.length - 1);
+        System.out.println(Arrays.toString(data));
+    }
+
+}
