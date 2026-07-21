f2774a78c29f7351790aa78f0ca31983cc03cd36
DongHwan Seo <0301sdh@naver.com>
2026-07-21 17:27:29 +0900

    make Heap Sort with java

--- Sort/HeapSort.java
+++ Sort/HeapSort.java
@@ -2,28 +2,21 @@ package Sort;
 
 public class HeapSort<T extends Comparable<T>> {
 
-    private Object[] heap;
     private int size;
-    private int capacity;
 
-    public HeapSort(T[] arr) {
-        this.capacity = arr.length;
+    public void sort(T[] arr) {
         this.size = arr.length;
-        this.heap = new Object[capacity];
-
-        for (int i = 0; i < size; i++) {
-            heap[i] = arr[i];
-        }
 
         for (int i = parent(size - 1); i >= 0; i--) {
-            siftDowm(i);
+            siftDown(arr, i);
         }
 
         while (size > 0) {
-            swap(0, size - 1);
-            siftDowm(0);
+            swap(arr, 0, size - 1);
             size--;
+            siftDown(arr, 0);
         }
+
     }
 
     private int parent(int i) {
@@ -38,40 +31,40 @@ public class HeapSort<T extends Comparable<T>> {
         return i * 2 + 2;
     }
 
-    @SuppressWarnings("unchecked")
-    private void siftDowm(int i) {
+    private void siftDown(T[] arr, int i) {
         while (true) {
             int largest = i;
             int left = leftChild(i);
             int right = rightChild(i);
 
-            if (left < size && ((T) heap[left]).compareTo((T) heap[largest]) > 0) {
+            if (left < size && arr[left].compareTo(arr[largest]) > 0) {
                 largest = left;
             }
 
-            if (right < size && ((T) heap[right]).compareTo((T) heap[largest]) > 0) {
+            if (right < size && arr[right].compareTo(arr[largest]) > 0) {
                 largest = right;
             }
 
             if (largest == i) {
                 break;
             }
-            swap(i, largest);
+            swap(arr, i, largest);
             i = largest;
         }
     }
 
-    private void swap(int i, int j) {
-        Object tmp = heap[i];
-        heap[i] = heap[j];
-        heap[j] = tmp;
+    private void swap(T[] arr, int i, int j) {
+        T tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 
     public static void main(String[] args) {
         Integer[] arr = { 3, 1, 9, 5, 2, 8, 7, 4 };
-        HeapSort<Integer> heapsort = new HeapSort<>(arr);
+        HeapSort<Integer> heapsort = new HeapSort<>();
+        heapsort.sort(arr);
         System.out.println("heap sort result");
-        for (int x : arr) {
+        for (Object x : arr) {
             System.out.print(x + " ");
         }
         System.out.println();
